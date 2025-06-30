c30240119a7268119ae349746cf8a25f1317fdd3
sorcerycss <sorcerycss@gmail.com>
2025-06-29 18:08:25 -0700

    Form submit event listener

--- index.js
+++ index.js
@@ -2,11 +2,25 @@ fetch("https://apis.scrimba.com/jsonplaceholder/posts")
   .then((res) => res.json())
   .then((data) => {
     const postsArr = data.slice(0, 5);
-    const itemsHTML = postsArr
+    const html = postsArr
       .map((post) => {
-        return `<h1>${post.title}</h1>
-                    <p>${post.body}</p>`;
+        return `<h2>${post.title}</h2>
+                    <p>${post.body}</p>
+                    <hr />`;
       })
       .join("");
-    document.getElementById("posts-array").innerHTML = itemsHTML;
+    document.getElementById("posts-array").innerHTML = html;
   });
+
+const infoCollector = document.getElementById("info-collector");
+
+infoCollector.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const postTitle = document.getElementById("post-title").value;
+  const postBody = document.getElementById("post-body").value;
+  const newPost = {
+    title: postTitle,
+    body: postBody,
+  };
+  console.log(newPost);
+});
